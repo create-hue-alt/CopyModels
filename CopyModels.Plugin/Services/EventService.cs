19f50d4c3329d09054354e6a457818215983df27
create-hue-alt <sevagurov@gmail.com>
2026-05-26 11:26:11 +0300

    feat: add architecture EventService

--- CopyModels.Plugin/Services/EventService.cs
+++ CopyModels.Plugin/Services/EventService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,60 @@ using System.Threading.Tasks;
 
 namespace CopyModels.Plugin.Services
 {
-    internal class EventService
+    /// <summary>
+    ///  Обработчики событий Revit: подавление диалогов и ошибок
+    /// </summary>
+    internal class EventService : IDisposable
     {
+        private readonly Application _app;
+        private readonly Action<string> _lofInfo;
+        private readonly Action<string> _logWarning;
+
+        private bool _subscribed;
+
+        public EventService(
+            Application app,
+            Action<string> logInfo = null,
+            Action<string> logWarning = null)
+        {
+            _app = app ?? throw new ArgumentNullException(nameof(app));
+            _lofInfo = logInfo ?? (_ => { });
+            _logWarning = logWarning ?? (_ => { });
+        }
+
+        // 
+        // Подписка/ отписка
+        // 
+
+        public void Subscribe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe() 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        public void Dispose() => Unsubscribe();
+
+        // 
+        // Обработчик ошибок
+        // 
+
+        private void OnFailureProcessing(object sender, FailuresProcessingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 
+        // Обработчик диалогов
+        // 
+
+        private void OnDialogBoxShowing(object sender, DialogBoxShowingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
