81c909904bcaab746ec022da3abc262c8837ca59
create-hue-alt <sevagurov@gmail.com>
2026-05-26 18:23:20 +0300

    feat: add architecture CopeModelsCommand

--- CopyModels.Plugin/CopyModelsCommand.cs
+++ CopyModels.Plugin/CopyModelsCommand.cs
@@ -1,12 +1,122 @@
-﻿using System;
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using CopyModels.Core.Models;
+using CopyModels.Plugin.Services;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CopyModels.Plugin
 {
-    public class CopyModelsCommand
+    /// <summary>
+    ///  Точка входа плагина.
+    /// </summary>
+    [TransactionAttribute(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class CopyModelsCommand : IExternalCommand
     {
+        private UIApplication _uiApp;
+        private FileService _fileService;
+        private RevitServerService _rsnService;
+        private ModelService _modelService;
+        private EventService _eventService;
+
+        // Накопленные результаты для итогового отчета
+        private Dictionary<string, List<string[]>> _resultTable;
+
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elementSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 
+        // Выполнение одного задания
+        // 
+
+        private void RunTask(
+            ProjectSettings task,
+            Action<string> logInfo,
+            Action<string> logWarning,
+            Action<string> logError)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 
+        // Построение списка ModelSetting
+        // 
+
+        private List<ModelSetting> BuildModelSettings(ProjectSettings task)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<string> BuildTargetPath(
+            string srcPath,
+            string srcDir,
+            ProjectSettings task)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 
+        // Обработка одной модели
+        // 
+
+        private void ProcessModel(
+            ModelSetting model,
+            ProjectSettings task,
+            Action<string> logInfo,
+            Action<string> logWarning,
+            Action<string> logError)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 
+        // Вспомогательные методы
+        // 
+
+        private void AddResult(
+            ProjectSettings task,
+            string model,
+            string action,
+            string from,
+            string to)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowResultReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> ShowSelectionDialog(
+            List<string> items,
+            string title,
+            bool multiselect)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteLog(StreamWriter writer, string level, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Мини-утилита для относительных путей 
+        internal static class PathUtils
+        {
+
+        }
+
     }
 }
