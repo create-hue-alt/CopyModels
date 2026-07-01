8e801c9985fed5d71af7dabab707a2ec12b6bdcf
create-hue-alt <sevagurov@gmail.com>
2026-07-01 11:37:26 +0300

    feat: add CopyModelsExecutor

---
+++ CopyModels.Plugin/CopyModelsExecutor.cs
@@ -0,0 +1,16 @@
+﻿using CopyModels.Plugin.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyModels.Plugin
+{
+    internal class CopyModelsExecutor
+    {
+        private readonly FileService _fileService;
+        private readonly RevitServerService _rsnService;
+        private readonly ModelService _modelService;
+    }
+}
